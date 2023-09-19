d530edfae9ef9b2854b998a423820c393eb76e8c
Jatan Jeyachandren <jatanjey@gmail.com>
2023-09-19 01:11:59 -0400

    app.js update

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-const inquirer = require("inquirer");
-const { startMenu } = require("./views/prompts");
+import("inquirer").then((inquirer) => {
+    const { startMenu } = require("./views/prompts");
 
-// Connect to the database and start the application
-const connection = require("./db/connection");
+    // Connect to the database and start the application
+    const connection = require("./db/connection");
 
-connection.connect((err) => {
-  if (err) throw err;
-  console.log("Connected to MySQL database");
-  startMenu();
-});
+    connection.connect((err) => {
+      if (err) throw err;
+      console.log("Connected to MySQL database");
+      startMenu();
+    });
+  })
+  .catch((error) => {
+    // Handle any errors that may occur during dynamic import
+    console.error("Error importing 'inquirer':", error);
+  });
+
